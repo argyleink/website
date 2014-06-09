b64ca14d507ced5136ba697546dda000223ea63a
Adam Argyle <atom@argyleink.com>
2014-06-09 12:22:35 -0700

    dynamic project loading

--- app/js/libs/page.js
+++ app/js/libs/page.js
@@ -6,30 +6,32 @@ var Page = (function() {
         speedIn : 100 
       });
 
-  $('body').on('click', '.pageload-link', function(ev){
-    ev.preventDefault();
+  function load(templateData) {
     loader.show();
-    jade.render($('#project')[0], 'project', {});
+    jade.render($('#project')[0], 'project', templateData);
+    $('#project-post').load('ajax/' + templateData.url);
     App.Home.disable();
     // after some time hide loader
-    setTimeout(function() {
-      App.Project = new Article($('#project')[0], {
-        trigger:  '#read'
-      });
-      loader.hide();
-
-      classie.removeClass( pages[ currentPage ], 'show' );
-      // update..
-      currentPage = currentPage ? 0 : 1;
-      classie.addClass( pages[ currentPage ], 'show' );
-
-      if (currentPage === 0)
-        App.Home.enable();
-
-    }, 2000);
-  });
-
-  return {
-
+    setTimeout(hideAndSwap, 2000);
   }
+
+  function hideAndSwap() {
+    App.Project = new Article($('#project')[0], {
+      trigger:  '#read'
+    });
+    loader.hide();
+
+    classie.removeClass( pages[ currentPage ], 'show' );
+    // update..
+    currentPage = currentPage ? 0 : 1;
+    classie.addClass( pages[ currentPage ], 'show' );
+
+    if (currentPage === 0)
+      App.Home.enable();
+  }
+
+  $('#grid').on('click', '> li', function(ev){
+    ev.preventDefault();
+    load(Projects[$(this).data().key]);
+  });
 })();
---
+++ app/js/projects.js
@@ -0,0 +1,128 @@
+var Projects = {
+  'lively': { 
+    key:    'lively',
+    image:  'http://placebox.es/600/400',
+    title:  'Lively',
+    detail: 'Cross platform responsive mobile first web app with native capabilities',
+    url:    'lively.html',
+    type:   'project',
+    article:{
+      cover: {
+        title:    'Lively',
+        subline:  'Cross Platform Native/Web Hybrid Mobile First Responsive App',
+        image:    ''
+      },
+      title: 'How & Why Mobile Web',
+      subline: 'Mobile web choices are complicated'
+    }
+  },
+  'nextissue': { 
+    image:  'http://placebox.es/600/400',
+    title:  'Next Issue Media',
+    detail: 'ActionScript 3 game built for iOS, Android, and BlackBerry phones and tablets',
+    url:    'dieantsdie.html',
+    type:   'project'
+  },
+  'github': { 
+    image:  'img/tiles/github.png',
+    title:  'github-alt',
+    url:    'https://github.com/argyleink',
+    type:   'social'
+  },
+  'clear': { 
+    image:  'http://placebox.es/600/400',
+    title:  'Clear',
+    detail: 'A Javascript PhoneGap HTML5 recreation backed by Nodejs & Mongo',
+    url:    'clear.html',
+    type:   'project'
+  },
+  'ubercute': { 
+    image:  'http://placebox.es/400/500',
+    title:  'Uber Cute Baby Animals',
+    detail: 'An app so cute, no one can withstand it.',
+    url:    'ubercute.html',
+    type:   'project'
+  },
+  'leap': { 
+    image:  'img/tiles/leap.png',
+    title:  'Leap Motion',
+    detail: 'Fun little projects',
+    url:    'leap.html',
+    type:   'project'
+  },
+  'dieantsdie': { 
+    image:  'http://placebox.es/700/400',
+    title:  'Die Ants Die!',
+    detail: 'ActionScript 3 game built for iOS, Android, and BlackBerry phones and tablets',
+    url:    'dieantsdie.html',
+    type:   'project'
+  },
+  'lessons': { 
+    image:  'http://placebox.es/600/400',
+    title:  'HTML/Javascript Lessons',
+    detail: 'I have open sourced a decent amount of work to help people learn and build better web sites.',
+    url:    'lessons.html',
+    type:   'project'
+  },
+  'icookbook': { 
+    image:  'http://placebox.es/600/400',
+    title:  'iCookbook',
+    detail: 'Recipe application for iOS, Android, Kindle, Win8, and more',
+    url:    'icookbook.html',
+    type:   'project'
+  },
+  'film': { 
+    image:  'http://placebox.es/600/400',
+    title:  'Film.com',
+    detail: 'Advanced Wordpress site',
+    url:    'film.html',
+    type:   'project'
+  },
+  'condenast': { 
+    image:  'http://placebox.es/400/600',
+    title:  'Conde Nast:GQ',
+    detail: 'Windows 8 WinJS Responsive Native App',
+    url:    'condenast.html',
+    type:   'project'
+  },
+  'storyville': { 
+    image:  'http://placebox.es/600/400',
+    title:  'Storyville',
+    detail: 'Coffee for a cause',
+    url:    'storyville.html',
+    type:   'project'
+  },
+  'marketleader': { 
+    image:  'http://placebox.es/600/400',
+    title:  'Market Leader',
+    detail: 'Real Estate Mobile Web App',
+    url:    'marketleader.html',
+    type:   'project'
+  },
+  'syntaxbros': { 
+    image:  'http://placebox.es/600/400',
+    title:  'Syntax Bros',
+    detail: 'ActionScript 3 game built for the developer',
+    url:    'syntaxbros.html',
+    type:   'project'
+  },
+  'visqueen': { 
+    image:  'http://placebox.es/600/400',
+    title:  'Visqueen',
+    detail: 'Awesome Seattle band.',
+    url:    'visqueen.html',
+    type:   'project'
+  },
+  'naturespace': { 
+    image:  'http://placebox.es/600/400',
+    title:  'Naturespace',
+    detail: '3D Sounds',
+    url:    'naturespace.html',
+    type:   'project'
+  },
+  'whymessage': {
+    title:    '"',
+    message:  'It is not what you do, it is why you do it.',
+    type:     'message'
+  }
+};

d9c523fa3f7c5971caa14c250dc4c7378715ae49
Adam Argyle <atom@argyleink.com>
2014-07-19 13:47:04 -0700

    enabling gzip on static file server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var static = require('node-static');
 
-var fileServer = new static.Server('/dist');
+var fileServer = new static.Server('/dist', { gzip: true });
 
 require('http').createServer(function (request, response) {
   request.addListener('end', function () {
